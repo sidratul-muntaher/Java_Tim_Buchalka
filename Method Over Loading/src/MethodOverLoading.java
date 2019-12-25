5df43ce05a197e874067f3d44037e7045a26dbcc
SIDTATUL <19103073@iubat.edu>
2019-12-25 14:03:03 +0600

    Empty parameter added

--- Method Over Loading/src/MethodOverLoading.java
+++ Method Over Loading/src/MethodOverLoading.java
@@ -3,6 +3,7 @@ public class MethodOverLoading {
         int newScore = calculateScore("Sid", 100);
         System.out.println("New Score is " + newScore);
         calculateScore(70);
+        calculateScore();
     }
     public  static int calculateScore(String playername, int Score){
         System.out.println("Player " + playername + " scored " + Score + " point ");
@@ -11,5 +12,9 @@ public class MethodOverLoading {
     public static int calculateScore(int score){
         System.out.println("Uname player scored " + score + " point");
         return score;
+    }
+    public static int calculateScore(){
+        System.out.println("no player name no player Score");
+        return 0;
     }
 }
