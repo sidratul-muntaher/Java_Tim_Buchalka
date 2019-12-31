cdf715ea8dffc9d71b481773c19584a74dd618bc
SIDTATUL <19103073@iubat.edu>
2019-12-31 19:58:54 +0600

    prime number type 1.1

--- PrimeNumber/src/PrimeNumber.java
+++ PrimeNumber/src/PrimeNumber.java
@@ -13,19 +13,15 @@ public class PrimeNumber {
 
     }
     public static boolean isPrime(int n){
-        int count = 0;
         if(n == 1){
             return false;
         }
-        for(int i = 1; i <= n; i++){
+        for(int i = 2; i <= n/2; i++){
             if(n % i == 0){
-                count ++;
+                return false;
             }
         }
-        if(count == 2){
-            return true;
-        }
-        return false;
+        return true;
 
     }
 }
